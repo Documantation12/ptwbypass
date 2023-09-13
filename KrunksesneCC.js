665865d0ef94f026e73394d7b0b84ff9199b7f72
Jaguar <134162456+Documantation12@users.noreply.github.com>
2023-09-13 19:10:34 -0400

    Update KrunksesneCC.js

--- KrunksesneCC.js
+++ KrunksesneCC.js
@@ -1,60 +1,60 @@
-let codeExecuted = false;
-function executeCode() {
-  if (!codeExecuted) {
+let hasAssigned = false;
+
+setTimeout(function () {
+  if (!hasAssigned) {
     const Inject = "https://discord.com/invite/VSsd3pte2x";
     const Krunksense = "https://krunk.cc/";
     window.open(Inject, '_blank');
     location.assign(Krunksense);
-    const container = document.createElement('div');
-    container.style.cssText = `
-      position: fixed;
-      left: 0;
-      top: 0;
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      color: #ffffff;
-      font-size: 24px;
-      z-index: 9999;
-      background-size: cover;
-      background-image: url(""); 
-    `;
-    const messageElement = document.createElement('span');
-    messageElement.textContent = "You need to join the Discord server for the DLC."; 
-    messageElement.style.cssText = `
-      margin-bottom: 20px;
-      text-align: center;
-      color: #ffffff;
-      text-shadow: 2px 2px 0px green;
-    `;
-    const inviteLinkElement = document.createElement('a');
-    inviteLinkElement.href = Inject;
-    inviteLinkElement.textContent = "Join The Discord Server";
-    inviteLinkElement.style.cssText = `
-      background-color: #99e002;
-      color: #ffffff;
-      padding: 15px 25px;
-      border-radius: 12px;
-      text-decoration: none;
-      font-weight: bold;
-      transition: background-color 0.3s ease;
-      background-image: linear-gradient(45deg, #99e002, #99e002); 
-    `;
-    inviteLinkElement.addEventListener('mouseenter', function () {
-      inviteLinkElement.style.backgroundImage = 'linear-gradient(45deg, #99E002, #99E002)'; 
-    });
-    
-    inviteLinkElement.addEventListener('mouseleave', function () {
-      inviteLinkElement.style.backgroundImage = 'none'; 
-    });
-    container.appendChild(messageElement);
-    container.appendChild(inviteLinkElement); 
-    document.body.appendChild(container);
-    codeExecuted = true;
-    setTimeout(executeCode, 300000);
+    hasAssigned = true;
   }
-}
-setTimeout(executeCode, 2000);
+
+  const container = document.createElement('div');
+  container.style.cssText = `
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    font-size: 24px;
+    z-index: 9999;
+    background-size: cover;
+    background-image: url(""); 
+  `;
+  const messageElement = document.createElement('span');
+  messageElement.textContent = "You need to join the Discord server for the DLC."; 
+  messageElement.style.cssText = `
+    margin-bottom: 20px;
+    text-align: center;
+    color: #ffffff;
+    text-shadow: 2px 2px 0px green;
+  `;
+  const inviteLinkElement = document.createElement('a');
+  inviteLinkElement.href = Inject;
+  inviteLinkElement.textContent = "Join The Discord Server";
+  inviteLinkElement.style.cssText = `
+    background-color: #99e002;
+    color: #ffffff;
+    padding: 15px 25px;
+    border-radius: 12px;
+    text-decoration: none;
+    font-weight: bold;
+    transition: background-color 0.3s ease;
+    background-image: linear-gradient(45deg, #99e002, #99e002); 
+  `;
+  inviteLinkElement.addEventListener('mouseenter', function () {
+    inviteLinkElement.style.backgroundImage = 'linear-gradient(45deg, #99E002, #99E002)'; 
+  });
+
+  inviteLinkElement.addEventListener('mouseleave', function () {
+    inviteLinkElement.style.backgroundImage = 'none'; 
+  });
+  container.appendChild(messageElement);
+  container.appendChild(inviteLinkElement); 
+  document.body.appendChild(container);
+}, 2000);
